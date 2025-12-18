ff56d7c874713e97964a8b349c8f4dc0147627fd
VitoAntonioPagone <vitopagone@outlook.com>
2025-12-18 12:50:27 +0100

    faicon fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,23 @@
 import Script from "next/script";
 import "./globals.css";
-export const icons = {
-  icon: [
-    { url: "/favicon_io/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-    { url: "/favicon_io/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-    { url: "/favicon_io/favicon.ico" },
-  ],
-  apple: [{ url: "/favicon_io/apple-touch-icon.png", sizes: "180x180" }],
-};
-
-export const manifest = "/favicon_io/site.webmanifest";
 
 export const metadata = {
   title: "La Quattordici - Luxury Ostuni Rental",
   description: "Ostuni • Puglia"
 };
 
+
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="scroll-smooth">
       <head>
+        <link rel="icon" href="/favicon_io/favicon.ico" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png" />
+      <link rel="manifest" href="/favicon_io/site.webmanifest" />
+
         <meta charSet="utf-8" />
         <meta content="width=device-width, initial-scale=1.0" name="viewport" />
 <link
