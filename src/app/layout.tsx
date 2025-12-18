e6d538a2c4a1a5dd0f0653edfff35c30c5141eaf
VitoAntonioPagone <vitopagone@outlook.com>
2025-12-18 12:42:01 +0100

    icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,15 @@
 import Script from "next/script";
 import "./globals.css";
+export const icons = {
+  icon: [
+    { url: "/favicon_io/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    { url: "/favicon_io/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    { url: "/favicon_io/favicon.ico" },
+  ],
+  apple: [{ url: "/favicon_io/apple-touch-icon.png", sizes: "180x180" }],
+};
+
+export const manifest = "/favicon_io/site.webmanifest";
 
 export const metadata = {
   title: "La Quattordici - Luxury Ostuni Rental",
